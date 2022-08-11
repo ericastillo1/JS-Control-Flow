81777788579eefed3792491f372e4b9065b303ab
Erica Castillo <erica@Ericas-iMac.local>
2022-08-10 20:32:30 -0500

    finished taking notes

--- script.js
+++ script.js
@@ -96,3 +96,78 @@ if (newAge >= 21) {
 };
 
 /* Loops */ console.log("Practice Loops");
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+};
+
+
+for ( let i = 0; i < 100; i+=10) {
+    console.log(i);
+};
+
+
+for ( let i = 100; i >= 0; i-=20) {
+    console.log(i);
+};
+
+
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " Is an even number");
+    } else {
+        console.log(i + " Is an odd number");
+    };
+};
+
+
+console.log("Practice Examples");
+// Write a for loop that prints all the number from 7 to 635.
+for ( i = 7; i <= 635; i++) {
+    console.log(i);
+};
+
+// Declare the variables const start = 0 and const limit = 100. Write a for loop that counts from the value of start to the value oflimit.
+const start = 0;
+const limit = 100;
+
+for ( i = start; i <= limit; i+=20) {
+    console.log(i);
+};
+
+// Write another for loop that counts from 1 to 100 , and console.logs each number that is a multiple of 3. (i.e. => 1, 3, 6, 9, 12, 15, ...)
+
+// The modulus operator - % - returns the remainder of a division operation.
+for ( i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
+        console.log(i);
+    };
+};
+
+/* Iterating over Arrays */ console.log("Practice Iterating over Arrays");
+let instructors = ["Don", "Hector", "Jimmy", "Zakk", "Eva", "Pearl", "Erin"];
+
+instructors.push("Erica");
+
+for ( let i = 0; i < instructors.length; i++) {
+    console.log(instructors[i].toUpperCase());
+};
+
+/* While Loop */ console.log("Practice While Loop");
+let num = 0;
+while (num <=10) {
+    console.log(num);
+    num++
+};
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+};
+
+
+/* let passwordGuess = "";
+
+while (passwordGuess !== "password") {
+    passwordGuess = prompt("You have been imprisoned in the code-block of a while loop! What is the magic word to exit?")
+};
+
+alert("Argh! You have escaped! I am so lonely, no one ever wants to stay.") */
